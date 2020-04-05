58951f623b68961092bcea698b51272ed19d0345
TakashiKitamura <monkeydolce@gmail.com>
2020-04-05 14:15:44 +0900

    firstcommit

---
+++ App.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { StyleSheet, Button, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+class HomeScreen extends React.Component {
+    render(){
+        return (
+            <View style={styles.container}>
+                <Text>Open up App.js to start working on your app!</Text>
+                <Text>Hello World!</Text>
+                <Button
+                    title="Go to Details"
+                    onPress={() => this.props.navigation.navigate('Details')}
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+class DetailsScreen extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Details Screen</Text>
+            </View>
+        );
+    }
+}
+
+const RootStack = createStackNavigator(
+    {
+        Home: HomeScreen,
+        Details: DetailsScreen,
+    },
+    {
+        initialRouteName: 'Home',
+    }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+    render() {
+        return <AppContainer />;
+    }
+}
